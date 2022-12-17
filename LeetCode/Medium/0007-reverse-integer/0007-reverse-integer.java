d532c5034575c7b9ab6e7c44a057225eb77618c9
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-12-17 14:50:55 +0900

    Time: 1 ms (100.00%), Space: 39.4 MB (92.53%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
@@ -1,7 +1,12 @@
 class Solution {
     public int reverse(int x) {
+        
+        /* 반환할 result 선언 */
         int result = 0;
+        
+        /* 반복문 */
         while(x != 0) {
+            
             int check = result * 10 + x % 10;
             
             if ((check - x % 10) / 10 != result) {
