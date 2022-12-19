286b37fe942df4d80686651a7de75f38f1c398ff
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-12-19 10:04:59 +0900

    Time: 1 ms (100.00%), Space: 39.3 MB (97.63%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
@@ -1,24 +1,32 @@
-import java.lang.StringBuilder;
-
 class Solution {
     public int reverse(int x) {
-        try {
-            String str;
-            StringBuilder sb;
         
-            if (x < 0) {
-                x = Math.abs(x);
-                str = Integer.toString(x);
-                sb = new StringBuilder(str);
+        /* 반환할 result 선언 */
+        int result = 0;
+        
+        /* 반복문 */
+        while(x != 0) {
             
-                return -Integer.parseInt(sb.reverse().toString());
+						/* 
+						 * 결과 * 10 : 자리 수 올려주기
+						 * x % 10 : 10으로 나누어 일의 자리수 나머지 더하기
+						 */ 
+            int check = result * 10 + x % 10;
+            
+						/*  검산 */ 
+            if ((check - x % 10) / 10 != result) {
+                return 0;
             }
-            str = Integer.toString(x);
-            sb = new StringBuilder(str);
-        
-            return Integer.parseInt(sb.reverse().toString());
-        } catch (NumberFormatException e) {
-            return 0;
+            
+						/* 
+						* x를 10으로 나누어 자리 수 줄이기
+						* 이미 x의 일의 자리 숫자는 나머지 값으로 구하여 더해 줌.
+						*/ 
+            x /= 10;
+            
+            result = check;
         }
+        
+        return result;
     }
 }
