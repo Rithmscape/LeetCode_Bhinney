86e72fb68ac3cd1f81a32bd82533ab80b219af40
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-12-22 13:50:15 +0900

    Time: 4 ms (95.31%), Space: 42.9 MB (79.87%) - LeetHub

---
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int num = getNum(c);
+
+            /* 만약 다음 수가 없다면 */
+            if (i + 1 == s.length()) {
+                sum += num;
+                continue;
+            }
+
+            /* 뒤에 오는 수가 지금 현재보다 크면 빼야함 */
+            char next = s.charAt(i + 1);
+            if (num < getNum(next)) {
+                sum -= num;
+            } else {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+
+    /* 비교할 수 있는 메서드 생성 */
+    public int getNum(char character) {
+        switch (character) {
+            case 'I': {return 1;}
+            case 'V': {return 5;}
+            case 'X': {return 10;}
+            case 'L': {return 50;}
+            case 'C': {return 100;}
+            case 'D': {return 500;}
+            case 'M': {return 1000;}
+
+            default: { return 1; }
+        }
+    }
+}
