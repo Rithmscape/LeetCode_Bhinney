bb66c7bce9de891988a4ddf8639082baab963cd1
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-12-22 14:21:58 +0900

    Time: 12 ms (40.00%), Space: 42.9 MB (77.56%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,39 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            int num = getNum(c);
+        String[] roman = {"I", "V", "X", "L", "C", "D", "M"};
+		String[] num = {"1 ", "5 ", "10 ", "50 ", "100 ", "500 ", "1000 "};
 
-            /* 만약 다음 수가 없다면 */
-            if (i + 1 == s.length()) {
-                sum += num;
-                continue;
-            }
+		for (int i = 0; i < roman.length; i++) {
+			s = s.replace(roman[i], num[i]);
+		}
 
-            /* 뒤에 오는 수가 지금 현재보다 크면 빼야함 */
-            char next = s.charAt(i + 1);
-            if (num < getNum(next)) {
-                sum -= num;
-            } else {
-                sum += num;
-            }
-        }
-        return sum;
-    }
+		String[] arr = s.split(" ");
 
-    /* 비교할 수 있는 메서드 생성 */
-    public int getNum(char character) {
-        switch (character) {
-            case 'I': {return 1;}
-            case 'V': {return 5;}
-            case 'X': {return 10;}
-            case 'L': {return 50;}
-            case 'C': {return 100;}
-            case 'D': {return 500;}
-            case 'M': {return 1000;}
+		int sum = 0;
 
-            default: { return 1; }
-        }
+		for (int i = 0; i < arr.length; i++) {
+			if (i + 1 == arr.length) {
+				sum += Integer.parseInt(arr[i]);
+				continue;
+			}
+
+			int now = Integer.parseInt(arr[i]);
+			int next = Integer.parseInt(arr[i + 1]);
+
+			sum = now < next ? sum - now : sum + now;
+
+		}
+
+		return sum;
     }
 }
