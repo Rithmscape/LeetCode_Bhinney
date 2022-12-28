f89b33d4a418e774bab4f10968b3fe16e73e5c24
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-12-28 10:17:43 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.54 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static int[] solution(int[] lottos, int[] win_nums) {
+		int[] answer = new int[2];
+
+		ArrayList<Integer> lotto = new ArrayList<>();
+		ArrayList<Integer> nums = new ArrayList<>();
+
+		for (int i = 0; i < lottos.length; i++) {
+			lotto.add(lottos[i]);
+			nums.add(win_nums[i]);
+		}
+
+		int count = 0;
+
+		for (int i = 0; i < lotto.size(); i++) {
+			if (nums.contains(lotto.get(i))) {
+				count++;
+			}
+		}
+
+		int cnt = (int)lotto.stream().filter(n -> n == 0).count();
+
+		if (cnt > 0) {
+			answer[0] = getRank(count + cnt);
+			answer[1] = getRank(count);
+		} else {
+			answer[0] = getRank(count);
+			answer[1] = getRank(count);
+		}
+
+
+		return answer;
+	}
+
+	public static int getRank(int num){
+		switch (num) {
+			case 6 : return 1;
+			case 5 : return 2;
+			case 4 : return 3;
+			case 3 : return 4;
+			case 2 : return 5;
+			default: return 6;
+		}
+	}
+}
