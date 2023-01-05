0a20ccb464b0047f3d61bd9c41003853126dbd37
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-01-05 10:55:57 +0900

    Time: 1 ms (59.90%), Space: 42.9 MB (43.44%) - LeetHub

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,11 +10,10 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // if(l1 == null) return l2;
-        // else if(l2 == null) return l1;
 
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
+
         while(l1 != null && l2!= null){
             if(l1.val <= l2.val){
                 curr.next = l1;
@@ -25,6 +24,7 @@ class Solution {
             }
             curr = curr.next;
         }
+
         curr.next = l1 == null? l2:l1;
         return dummy.next;
     }
