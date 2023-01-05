17d57c80e26590758384fc2f8a8ece410a43facf
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-01-05 11:26:18 +0900

    Time: 1 ms (96.04%), Space: 42.6 MB (63.35%) - LeetHub

--- LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.java
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,18 +4,21 @@ import java.util.List;
 class Solution {
     public static List<String> generateParenthesis(int n) {
 		List<String> result = new ArrayList<>();
-
-		if (n == 0) {
-			result.add("");
-		}
-
-		for (int c = 0; c < n; ++c) {
-			for (String left: generateParenthesis(c)) {
-				for (String right: generateParenthesis(n-1-c))
-					result.add("(" + left + ")" + right);
-			}
-		}
-
+		backtrack(result, "", 0, 0, n );
 		return result;
+	}
+	private static void backtrack (List<String> list, String str, int start, int end, int n) {
+
+		if (str.length() == n * 2) {
+			list.add(str);
+			return;
+		}
+
+		if (start < n) {
+			backtrack(list, str + "(", start + 1, end, n);
+		}
+		if (end < start) {
+			backtrack(list, str + ")", start, end + 1, n);
+		}
 	}
 }
