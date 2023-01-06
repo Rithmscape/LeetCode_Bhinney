8814b2567fff9d1386fe89f76d7e7a55121d00d7
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-01-06 10:56:01 +0900

    Time: 785 ms (43.62%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ LeetCode/Easy/1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
 
-SELECT employee_id,
-IF (employee_id % 2 AND name not like "M%", salary, 0) as bonus
-FROM Employees order by employee_id;
+# SELECT employee_id,
+# IF (employee_id % 2 AND name not like "M%", salary, 0) as bonus
+# FROM Employees order by employee_id;
+
+select 
+    employee_id, 
+    salary * (employee_id % 2) * (name not like "M%") as bonus 
+from Employees order by employee_id;
