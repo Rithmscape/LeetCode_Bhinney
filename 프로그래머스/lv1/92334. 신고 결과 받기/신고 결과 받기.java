d293268e744fcb27ab9855e40de33319df919049
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-01-26 15:38:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.18 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,38 +4,23 @@ import java.util.HashSet;
 
 class Solution {
     public static int[] solution(String[] id_list, String[] report, int k) {
-		int[] result = new int[id_list.length];
-
-		HashSet<String> set = new HashSet<>(report.length);
+		HashSet<String> set = new HashSet<>();
 		set.addAll(Arrays.asList(report));
-		HashMap<String, HashSet<String>> reporting = new HashMap<>();
-
+		HashMap<String, Integer> reporting = new HashMap<>();
 		for (String str : set) {
-			String reporter = str.split(" ")[0]; // 신고자
-			String reported = str.split(" ")[1]; // 신고 당한 사람
-
-			if (reporting.containsKey(reported)) {
-				HashSet<String> value = reporting.get(reported);
-				value.add(reporter);
-				reporting.put(reported, value);
-			} else {
-				HashSet<String> value = new HashSet<>();
-				value.add(reporter);
-				reporting.put(reported, value);
-			}
+			String target = str.split(" ")[1];
+			reporting.put(target, reporting.getOrDefault(target, 0) + 1);
 		}
 
 		HashMap<String, Integer> mail = new HashMap<>();
-		for (int i = 0; i < id_list.length; i++) {
-			String key = id_list[i];
-			if (reporting.containsKey(key) && reporting.get(key).size() >= k) {
-				HashSet<String> value = reporting.get(key);
-				for (String str : value) {
-					mail.put(str, mail.getOrDefault(str, 0) + 1);
-				}
+		for (String str : set) {
+			String target = str.split(" ")[1];
+			String reporter = str.split(" ")[0];
+			if (reporting.containsKey(target) && reporting.get(target) >= k) {
+				mail.put(reporter, mail.getOrDefault(reporter, 0) + 1);
 			}
 		}
-
+		int[] result = new int[id_list.length];
 		for (int i = 0; i < id_list.length; i++) {
 			result[i] = mail.getOrDefault(id_list[i], 0);
 		}
