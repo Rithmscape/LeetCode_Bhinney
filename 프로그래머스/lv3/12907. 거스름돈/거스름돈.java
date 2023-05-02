511b2701dbe07ac583f36667cd4b1bb0e99eafe7
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-05-02 19:34:11 +0900

    [level 3] Title: 거스름돈, Time: 12.73 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        
+        for (int c : money) {
+            for (int i = 1; i <= n; i++) {
+				if (i - c >= 0) {
+					dp[i] += dp[i - c];
+				}
+			}
+        }
+        
+        return dp[n];
+    }
+}
