4332a7cd92a74a2708f5c0ab6527fdb57b74879c
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-05-30 20:19:00 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 66.01 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,80 +1,26 @@
 import java.util.*;
+import java.util.regex.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        
-        File[] info = new File[files.length];
-        
-        for (int i = 0; i < files.length; i++) {
-            String[] str = splitFile(files[i]);
-            info[i] = new File(str[0], Integer.parseInt(str[1]), str[2], files[i]);
-        }
-        
-        Arrays.sort(info, new Comparator<File>() {
+        Pattern p = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
+
+        Arrays.sort(files, new Comparator<String>() {
             @Override
-             public int compare(File o1, File o2) {
-                 if ((o1.head).equals(o2.head)) {
-                    return o1.number-o2.number;
-                }else{
-                    return (o1.head).compareTo(o2.head);
+            public int compare(String s1, String s2) {
+                Matcher m1 = p.matcher(s1.toLowerCase());
+                Matcher m2 = p.matcher(s2.toLowerCase());
+                m1.find();
+                m2.find();
+
+                if(!m1.group(1).equals(m2.group(1))) {
+                    return m1.group(1).compareTo(m2.group(1));
+                } else {
+                    return Integer.parseInt(m1.group(2)) - Integer.parseInt(m2.group(2));
                 }
-             }
+            }
         });
-        
-        String[] result = new String[info.length];
-        
-        for (int i = 0; i < info.length; i++) {
-            result[i] = info[i].full;
-        }
-        
-        return result;
-    }
-    
-    public String[] splitFile(String file) {
-        String[] str = new String[3];
-        str[0] = str[1] = str[2] = ""; /* 차례대로 head, number, tail*/
-        
-        int idx = 0;
-        
-        for (int i = 0; i < file.length(); i++) {
-            char c = file.charAt(i);
-            
-            if (idx == 0 && !Character.isDigit(c)) {
-                str[idx] += c;
-                continue;
-            }
-            
-            if(idx == 0 && Character.isDigit(c)){
-                idx++;
-            }
 
-            if(idx == 1 && Character.isDigit(c)){
-                str[idx] += c;
-                continue;
-            }
-
-            if(idx == 1 && !Character.isDigit(c)){
-                idx++;
-            }
-            str[idx] += c;
-        }
-        
-        str[0] = str[0].toLowerCase();
-        
-        return str;
+        return files;
     }
-    
-    public class File {
-    String head;
-    int number;
-    String tail;
-    String full;
-    
-    File (String head, int number, String tail, String full) {
-        this.head = head;
-        this.number = number;
-        this.tail = tail;
-        this.full = full;
-    }
-}
 }
