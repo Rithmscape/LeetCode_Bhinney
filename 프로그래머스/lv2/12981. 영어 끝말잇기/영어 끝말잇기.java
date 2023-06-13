d7e08141c4742be4d0e3f3b626a3747461874133
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-06-13 11:06:23 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        List<String> list = new ArrayList<>();
+        int[] answer = {0, 0};
+        list.add(words[0]);
+        
+        for (int i = 1; i < words.length; i++) {
+            String last = words[i - 1];
+            String now = words[i];
+            
+            if (last.charAt(last.length() - 1) != now.charAt(0) || list.contains(now)) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                
+                return answer;
+            }
+            
+            list.add(now);
+        }
+        
+        return answer;
+    }
+}
