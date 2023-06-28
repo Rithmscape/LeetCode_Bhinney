624c166f0d24e0d2d549fcffb2e55c9a05184279
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-06-28 16:06:13 +0900

    [level 2] Title: [1차] 캐시, Time: 2.91 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int size, String[] cities) {
+        
+        if (size == 0) {
+            return cities.length * 5;
+        }
+
+        int ans = 0;
+        Queue<String> cache = new LinkedList<>();
+        
+        for (String city : cities) {
+            city = city.toLowerCase();
+            
+            if (cache.contains(city)) {
+                cache.remove(city);
+                ans += 1;
+            } else {
+                if(cache.size() >= size) {
+                    cache.poll();
+                }
+                ans += 5;
+            }
+            
+            cache.offer(city);
+        }
+        
+        return ans;
+    }
+}
