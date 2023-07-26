fc199fc9397bd4563c10a281e3d332401c07522d
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-07-26 18:58:19 +0900

    [level 1] Title: 하샤드 수, Time: 1.51 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
-    public boolean solution(int x) {
-        int h = 0;
-        int a = x;
-        while (a > 0) {
-            h += a % 10;
-            a /= 10;
-        }
-        
-        return x % h == 0 ? true : false;
-    }
+    public static boolean solution(int x) {
+		String[] arr = String.valueOf(x).split("");
+		int h = Arrays.stream(arr).mapToInt(Integer::parseInt).sum();
+		return x % h == 0 ? true : false;
+	}
 }
