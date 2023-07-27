68751439282da4a1b202db7859a240521cb13ebc
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-07-27 19:01:10 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.20 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -4,6 +4,6 @@ class Solution {
     public String solution(String[] seoul) {
         int idx = Arrays.asList(seoul).indexOf("Kim");
         
-        return new StringBuilder().append("김서방은 ").append(idx).append("에 있다").toString();
+        return "김서방은 " + idx + "에 있다";
     }
 }
