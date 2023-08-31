e04a19493a080b584199f5b9b7b43ae92f27f4dd
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-08-31 17:40:58 +0900

    [unrated] Title: 옹알이 (2), Time: 0.61 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,25 @@
+class Solution {
+    static String[] possible = {"aya", "ye", "woo", "ma"};
+	static String[] doubleString = {"ayaaya", "yeye", "woowoo", "mama"};
+	public static int solution(String[] babbling) {
+		int ans = 0;
+
+		for (String b : babbling) {
+
+			/* 연속된 발음은 할 수 없음 */
+			if (b.contains(doubleString[0]) || b.contains(doubleString[1]) || b.contains(doubleString[2]) || b.contains(doubleString[3]) || b.length() == 0) {
+				continue;
+			}
+
+			b = b.replace(possible[0], " ");
+			b = b.replace(possible[1], " ");
+			b = b.replace(possible[2], " ");
+			b = b.replace(possible[3], " ");
+			b = b.replace(" ", "");
+
+			if (b.length() == 0) ans++;
+		}
+
+		return ans;
+	}
+}
