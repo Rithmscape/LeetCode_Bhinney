23e1946004561a1ed128704daad213b3287b2b6b
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-09-11 19:47:13 +0900

    #18 🐈‍⬛ Programmers 문제 풀이 (#134)
    * [level 3] Title: 불량 사용자, Time: 1.23 ms, Memory: 73.4 MB -BaekjoonHub
    
    * [level 2] Title: [3차] 방금그곡, Time: 14.99 ms, Memory: 81.3 MB -BaekjoonHub
    
    * [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 78.34 ms, Memory: 82.5 MB -BaekjoonHub
    
    * [level 3] Title: 가장 먼 노드, Time: 51.90 ms, Memory: 107 MB -BaekjoonHub
    
    * [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub
    
    * [level 1] Title: 소수 찾기, Time: 177.18 ms, Memory: 51.9 MB -BaekjoonHub
    
    * [unrated] Title: 옹알이 (2), Time: 0.61 ms, Memory: 79 MB -BaekjoonHub
    
    * [unrated] Title: 옹알이 (2), Time: 0.57 ms, Memory: 78.1 MB -BaekjoonHub
    
    * [level 1] Title: 숫자 짝꿍, Time: 0.08 ms, Memory: 69.5 MB -BaekjoonHub
    
    * [unrated] Title: 대충 만든 자판, Time: 1.49 ms, Memory: 73.2 MB -BaekjoonHub
    
    * [unrated] Title: 둘만의 암호, Time: 0.88 ms, Memory: 75.8 MB -BaekjoonHub
    
    * [unrated] Title: 둘만의 암호, Time: 0.43 ms, Memory: 76.9 MB -BaekjoonHub
    
    * [level 1] Title: 신규 아이디 추천, Time: 1.69 ms, Memory: 74.7 MB -BaekjoonHub
    
    * [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.59 ms, Memory: 72.4 MB -BaekjoonHub
    
    * [unrated] Title: 덧칠하기, Time: 1.05 ms, Memory: 76.6 MB -BaekjoonHub
    
    * [unrated] Title: 기사단원의 무기, Time: 1.32 ms, Memory: 67.8 MB -BaekjoonHub
    
    * [level 1] Title: 실패율, Time: 2.59 ms, Memory: 72.6 MB -BaekjoonHub
    
    * [level 1] Title: 실패율, Time: 2.69 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    /*
+	 * N : 스테이지 개수, 1이상 500이하의 자연수
+	 * stages : 게임을 하는 사용자가 멈춰있는 스테이지, 1 이상 N + 1 이하의 자연수
+	 * 실패율이 높은 스테이지부터 내림차순으로 정렬
+	 */
+	public int[] solution(int N, int[] stages) {
+		/* 각 스테이지 별로 성공한 유저 카운팅 */
+		double[] success = new double[N + 1];
+		success[0] = stages.length;
+		for (int now : stages) {
+			for (int j = 1; j < now; j++) {
+				success[j]++;
+			}
+		}
+
+		/* 각 스테이지 별 실패율 담을 Map*/
+		Map<Integer, Double> fail = new HashMap<>();
+		for (int i = 1; i < success.length; i++) {
+			if (success[i - 1] == 0) {
+				fail.put(i, 0.0);
+			} else {
+				double d = (success[i - 1] - success[i])/ success[i - 1];
+				fail.put(i, d);
+			}
+		}
+
+		List<Integer> ans = new ArrayList<>(fail.keySet());
+		ans.sort(((o1, o2) -> Double.compare(fail.get(o2), fail.get(o1))));
+
+		return ans.stream().mapToInt(n -> n).toArray();
+	}
+}
