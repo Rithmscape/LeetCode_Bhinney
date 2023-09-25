ce57eb0641a6c07745f68023501773c075e49bdd
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-09-25 19:42:02 +0900

    [unrated] Title: 달리기 경주, Time: 0.24 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -1,24 +1,56 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] players, String[] callings) {
-       Map<String, Integer> runner = new HashMap<>();
+    public static String[] solution(String[] players, String[] callings) {
+		Player[] runner = new Player[players.length];
+		Map<String, Player> map = new HashMap<>();
 
 		for (int i = 0; i < players.length; i++) {
-			runner.put(players[i], i);
+			Player p = new Player(players[i], i);
+			runner[i] = p;
+			map.put(players[i], p);
 		}
 
 		for (String c : callings) {
-			int idx = runner.get(c);
-			String ahead = players[idx - 1];
+			Player now = map.get(c);
+			int nowRank = now.rank;
+			Player ahead = runner[nowRank - 1];
 
-			players[idx] = ahead;
-			players[idx - 1] = c;
+			now.setRank(c, nowRank - 1);
+			ahead.setRank(ahead.name, nowRank);
 
-			runner.put(ahead, idx);
-			runner.put(c, idx - 1);
+			runner[nowRank] = ahead;
+			runner[nowRank - 1] = now;
+
+			map.put(ahead.name,  ahead);
+			map.put(c, now);
 		}
 
-		return players;
-    }
+		String[] ans = new String[players.length];
+		for (int i = 0; i < ans.length; i++) {
+			ans[i] = runner[i].name;
+		}
+
+		return ans;
+	}
+
+	private static class Player {
+		String name;
+		int rank;
+
+		public Player(String name, int rank) {
+			this.name = name;
+			this.rank = rank;
+		}
+
+		public String getName(int rank) {
+			return name;
+		}
+
+
+		public void setRank(String name, int rank) {
+			this.name = name;
+			this.rank = rank;
+		}
+	}
 }
