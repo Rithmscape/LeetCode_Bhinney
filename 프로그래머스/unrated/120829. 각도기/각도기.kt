a02c113ef1ddacd563af25bd94cb7c980173cb68
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-10-23 16:22:51 +0900

    [level unrated] Title: 각도기, Time: 0.01 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(angle: Int): Int {
+        if (angle < 90) return 1
+        if (angle == 90) return 2
+        if (angle < 180) return 3
+        if (angle == 180) return 4
+        
+        return 0
+    }
+}
