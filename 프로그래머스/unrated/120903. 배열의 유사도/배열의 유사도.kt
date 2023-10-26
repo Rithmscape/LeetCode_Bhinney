714e93c7b6d66964d78b929b7b89cb366a2cf382
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-10-26 19:30:52 +0900

    [level unrated] Title: 배열의 유사도, Time: 14.88 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.kt
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(s1: Array<String>, s2: Array<String>): Int {
-        var answer: Int = 0
-
-        for (i : Int in 0 .. s1.size - 1) {
-            if (s2.contains(s1[i])) answer++;
-        }
-
-        return answer
-    }
+    fun solution(s1: Array<String>, s2: Array<String>): Int = s1.count { s2.contains(it) }
 }
