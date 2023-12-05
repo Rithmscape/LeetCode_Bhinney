0be3ea726dbbb43cd14f7ef2cad39ae90d9c1d6c
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-12-05 10:44:17 +0900

    [level unrated] Title: 조건 문자열, Time: 1.69 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/README.md
+++ 프로그래머스/unrated/181934. 조건 문자열/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 조건 문자열 - 181934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 0.02 ms
+메모리: 77.6 MB, 시간: 1.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 10:35:3
+2023년 12월 2일 10:44:15
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -2,16 +2,11 @@ class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         boolean check = true;
 
-		if (ineq.equals(">")) {
-			switch (eq) {
-				case "!" -> check = n > m;
-				case "=" -> check = n >= m;
-			}
-		} else {
-			switch (eq) {
-				case "!" -> check = n < m;
-				case "=" -> check = n <= m;
-			}
+		switch (ineq + eq) {
+			case ">=" -> check = n >= m;
+			case ">!" -> check = n > m;
+			case "<=" -> check = n <= m;
+			case "<!" -> check = n < m;
 		}
 
 		return check ? 1 : 0;
