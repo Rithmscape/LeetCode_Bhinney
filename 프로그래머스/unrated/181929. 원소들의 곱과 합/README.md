330bd780f9e757c0b3f893d8805e7a8fa2a94187
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-12-12 20:50:37 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 1.16 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/README.md
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 원소들의 곱과 합 - 181929 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181929?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181929?language=java) 
 
 ### 성능 요약
 
-메모리: 61.9 MB, 시간: 13.80 ms
+메모리: 71.6 MB, 시간: 1.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 20:48:44
+2023년 12월 2일 20:50:36
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,12 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] num_list) {
-        int multiply = num_list[0];
-		int sum = num_list[0];
-		for (int i = 1; i < num_list.length; i++) {
-			sum += num_list[i];
-			multiply *= num_list[i];
-		}
-		sum = (int)Math.pow(sum, 2);
+        int multiply = IntStream.of(num_list).reduce((a, b) -> a * b).orElse(0);
+		int sum = (int) Math.pow(IntStream.of(num_list).sum(), 2);
 
 		return multiply < sum ? 1 : 0;
     }
