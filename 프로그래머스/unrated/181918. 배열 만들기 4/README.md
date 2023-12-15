7f8c3648e3112ac09953703468a815527b793843
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-12-15 19:46:17 +0900

    [level unrated] Title: 배열 만들기 4, Time: 6.39 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/README.md
+++ 프로그래머스/unrated/181918. 배열 만들기 4/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 배열 만들기 4 - 181918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181918?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181918?language=java) 
 
 ### 성능 요약
 
-메모리: 62.8 MB, 시간: 10.01 ms
+메모리: 89.2 MB, 시간: 6.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 19:43:24
+2023년 12월 5일 19:46:16
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,21 +1,16 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
 		int i = 0;
+		
 		while (i < arr.length) {
-			if (stk.isEmpty()) {
-				stk.add(arr[i]);
-				i++;
-			} else if (stk.get(stk.size() - 1) < arr[i]) {
-				stk.add(arr[i]);
-				i++;
-			} else {
-				stk.remove(stk.size() - 1);
-			}
+			if (stk.isEmpty()) stk.add(arr[i++]);
+			else if (stk.peek() < arr[i]) stk.add(arr[i++]);
+			else stk.pop();
 		}
-
+		
 		return stk.stream().mapToInt(n -> n).toArray();
     }
 }
