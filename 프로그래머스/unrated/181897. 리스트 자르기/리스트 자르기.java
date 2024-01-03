5cf32c6ef7148a6dc45e1d95500953ae107b003f
Bhinney <107696895+Bhinney@users.noreply.github.com>
2024-01-03 22:15:33 +0900

    [level unrated] Title: 리스트 자르기, Time: 2.83 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        switch (n) {
+			case 1 -> {
+				return Arrays.copyOfRange(num_list, 0, slicer[1] + 1);
+			}
+			case 2 -> {
+				return Arrays.copyOfRange(num_list, slicer[0], num_list.length);
+			}
+			case 3 -> {
+				return Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1);
+			}
+			default -> {
+				ArrayList<Integer> ans = new ArrayList<>();
+				for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+					ans.add(num_list[i]);
+				}
+
+				return ans.stream().mapToInt(i -> i).toArray();
+			}
+		}
+    }
+}
