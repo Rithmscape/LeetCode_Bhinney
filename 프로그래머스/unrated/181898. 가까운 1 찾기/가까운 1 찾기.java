ccbf01bd3bf2e58cdeb108d6bfae33f9b0e80321
Bhinney <107696895+Bhinney@users.noreply.github.com>
2024-01-03 21:35:41 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 2.83 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,10 +1,7 @@
 import java.util.stream.IntStream;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] arr, int idx) {
-		int num = IntStream.of(arr).boxed().collect(Collectors.toList()).subList(idx, arr.length).indexOf(1);
-
-		return num == -1 ? -1 : num + idx;
+        return IntStream.range(idx, arr.length).filter(i -> arr[i] == 1).findFirst().orElse(-1);
     }
 }
