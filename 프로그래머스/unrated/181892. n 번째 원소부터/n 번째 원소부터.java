2ab61f1bd5f8c21eaced40a6c79f8395a121289a
Bhinney <107696895+Bhinney@users.noreply.github.com>
2024-01-04 20:13:29 +0900

    [level unrated] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,9 +1,8 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-                
-        return Arrays.stream(num_list).boxed().collect(Collectors.toList()).subList(n - 1, num_list.length).stream().mapToInt(i -> i).toArray();
+        
+        return Arrays.copyOfRange(num_list, n - 1, num_list.length);
     }
 }
