f3f28b528f1b3a93bef3d2af813dc3448c1e1018
Bhinney <107696895+Bhinney@users.noreply.github.com>
2024-01-23 20:58:42 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 120 ms, Memory: 13248 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    var n = nextInt()
+    var cnt = 0
+
+    while (n >= 5) {
+        cnt += n / 5
+        n /= 5
+    }
+
+    println(cnt)
+}
