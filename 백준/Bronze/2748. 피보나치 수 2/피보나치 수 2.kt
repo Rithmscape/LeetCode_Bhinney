bb47eedf64116cc2c2f020ed038da1804615e56c
Bhinney <107696895+Bhinney@users.noreply.github.com>
2024-02-07 19:15:01 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 120 ms, Memory: 13256 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
@@ -2,8 +2,8 @@ import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)){
     val n = nextInt()
-    val dp = Array(n + 1) {0L}
-    dp[1] = 1L;
+    val dp = LongArray(n + 1)
+    dp[1] = 1;
     for (i in 2 .. n) {
         dp[i] = dp[i - 1] + dp[i - 2]
     }
